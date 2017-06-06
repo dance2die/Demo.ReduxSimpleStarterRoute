9b7656f932e1959988b33036076bb4241906d57b
Sung <sungmkim80@gmail.com>
2017-06-06 19:26:01 -0400

    added 3 fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,36 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input 
+                    className="form-control"
+                    type="text"
+                    // instead of
+                    // onChange={field.input.onChange}
+                    // onFocus={field.input.onFocus}
+                    // onBlur={field.input.onBlur}
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div>
-                PostsNew!
-            </div>
+            <form>
+                <Field name="title" label="Title" component={this.renderField} />
+                <Field name="tags" label="Tags" component={this.renderField} />
+                <Field name="content" label="Post Content" component={this.renderField} />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+// this enables the redux form communicate with the reducer
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
