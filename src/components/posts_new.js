303e1e2b7aaebdabbc3c873df53d37ed427e675f
Sung <sungmkim80@gmail.com>
2017-06-07 19:23:20 -0400

    styled error message

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,8 +3,11 @@ import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
     renderField(field) {
+        const {meta: {touched, error}} = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input 
                     className="form-control"
@@ -15,7 +18,9 @@ class PostsNew extends Component {
                     // onBlur={field.input.onBlur}
                     {...field.input}
                 />
-                {field.meta.touched ? field.meta.error : ''}
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
         );
     }
