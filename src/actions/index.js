f24e361e5b5588ead567c640af63865d72d9ef0f
Sung <sungmkim80@gmail.com>
2017-06-08 20:16:08 -0400

    completely implemented this module

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POSTS = 'create_post';
 export const FETCH_POST = 'fetch_post';
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = "?key=dance2die1234"
@@ -42,3 +43,13 @@ export function fetchPost(id) {
     }
 }
 
+
+export function deletePost(id, callback) {
+    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+        .then(() => callback());
+
+    return {
+        type: DELETE_POST,
+        payload: id
+    }
+}
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,15 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function (state = {}, action) {
     switch (action.type) {
+        case DELETE_POST:
+            // this line removes a post in "state" 
+            // where the ID matches the value returned from "action.payload"
+            // "action.payload" contains a post ID from "actions/index.js/deletePost"
+
+            // This line cleans the state by removing deleted record from the memory
+            return _.omit(state, action.payload);
         case FETCH_POST:
             // const post = action.payload.data;
             // const newState =  {...state};
